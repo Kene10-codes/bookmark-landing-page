36c136b0b33273214356bb077ab29de428d67bf8
Kene10-codes <nwobodokenechukwu2@gmail.com>
2026-02-08 17:43:48 +0100

    functionality to close the nav links once any link is clicked

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const hamburger= document.getElementById("hamburger");
 const navLinks = document.getElementById("menu");
+const links = document.querySelectorAll(".nav-links a");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+})
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
